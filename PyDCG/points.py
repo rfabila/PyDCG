7572f938c2c41da109457a3c1742b82ebba0e700
Ruy Fabila <ruy@ciencias.unam.mx>
2015-07-03 16:14:32 -0500

    Added the functions to submit new point sets.

--- PyDCG/points.py
+++ PyDCG/points.py
@@ -21,9 +21,15 @@
    It also provides access to Graz's order point database for n=6,7,8,9,10"""
 import struct
 import geometricbasics
+import crossing
 import math
 import os
 import pickle
+import datetime
+import ConfigParser
+import random
+import string
+import holes
 
 def horton_set(n):
     """Returns a set of n points with the same order type
@@ -310,8 +316,10 @@ def overmars_sets():
     return P
 
 #Point set zoo functions. This are the best sets that optimize a certain parameter.
+#The format is {n:{"val":xxxx},{"pts",xxxx},{"user",xxxx},{"comment",}}
 def read_species(name):
-    file_pts=open("poin_sets/"+name+".pkl","r")
+    name = os.path.join(os.path.dirname(__file__),"point_sets/"+name+".pkl")
+    file_pts=open(name,"r")
     D=pickle.load(file_pts)
     file_pts.close()
     return D
@@ -324,17 +332,74 @@ def best_specimen(name,n):
 #functions for specific species
 
 def best_rectilinear_crossing_number_pts(n):
+    """Returns the best known example for rectilinear crossing number."""
     return best_specimen("rectilinear_crossing_number",n)
 
 def best_empty_convex_pentagons_pts(n):
+    """Returns the best known example for empty convex pentagons."""
     return best_specimen("empty_convex_pentagons",n)
 
 def best_empty_triangles_pts(n):
+    """Returns the best known example for empty triangles."""
     return best_specimen("empty_triangles",n)
 
 def best_empty_convex_quadrilaterals_pts(n):
+    """Returns the best known example for empty convex quadrilaterals."""
     return best_specimen("empty_convex_quadrilaterals",n)
 
 def best_empty_convex_hexagons_pts(n):
+    """Returns the best known example for empty convex hexagons."""
     return best_specimen("empty_convex_hexagons",n)
-    
+
+#submitting functions
+def _submit_point_set(pts,species,f,comment=" ",user_id=None):
+
+    if not geometricbasics.general_position(pts):
+        #this should probably be an exception
+        print "Point set not in general position!! I ain't sending nothing."
+
+        return None
+    sp={}
+    sp['comment']=comment
+    sp['val']=f(pts)
+    sp['pts']=pts
+    date_discovered=datetime.datetime.today()
+    sp['date_discovered']=date_discovered
+    if user_id==None:
+        config=ConfigParser.RawConfigParser()
+        home=os.path.expanduser("~")
+        print home+'.pydcg/pydcg.cfg'
+        config.read(home+'/.pydcg/pydcg.cfg')
+        user_id=config.get('user info','user_id')
+        print user_id
+    sp['user_id']=user_id
+    sp['species']=species
+
+    #to avoid collisions
+    idx=s=''.join(random.choice(string.ascii_letters + string.digits) for _ in range(10))
+    name=str(len(pts))+"_"+species+"_"+str(date_discovered.date())+"_"+idx+".sp"
+    file_sp=open(name,"w")
+    pickle.dump(sp,file_sp)
+    file_sp.close()
+    com="scp "+name+" naturalist@monk.math.cinvestav.mx:~/naturalist/captured_specimens/"
+    os.system(com)
+    #print com
+    os.system("rm "+name)
+
+#submitting functions for specific species
+def submit_rectilinear_crossing_number(pts,user_id=None,comment=None):
+    _submit_point_set(pts,"rectilinear_crossing_number",crossing.count_crossings,comment=comment,user_id=user_id)
+
+def submit_empty_convex_pentagons(pts,user_id=None,comment=None):
+    _submit_point_set(pts,"empty_convex_pentagons",holes.count_convex_rholes_maker(5),comment=comment,user_id=user_id)
+
+def submit_empty_triangles(pts,user_id=None,comment=None):
+    _submit_point_set(pts,"empty_triangles",holes.countEmptyTriangs,comment=comment,user_id=user_id)
+
+def submit_empty_convex_quadrilaterals(pts,user_id=None,comment=None):
+    _submit_point_set(pts,"empty_convex_quadrilaterals",holes.count_convex_rholes_maker(4),comment=comment,user_id=user_id)
+
+def submit_empty_convex_hexagons(pts,user_id=None,comment=None):
+    _submit_point_set(pts,"empty_convex_hexagons",holes.count_convex_rholes_maker(6),comment=comment,user_id=user_id)
+
+
